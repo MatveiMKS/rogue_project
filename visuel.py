4e82e764877aa3f342980be0e244a12233363282
MatveiBG <legomegamat@gmail.com>
2023-05-20 01:02:14 +0200

    creation of a class for sprites

--- visuel.py
+++ visuel.py
@@ -1,4 +1,5 @@
 import pygame
+from Char import Char
 
 pygame.init()
 
@@ -11,10 +12,16 @@ window = pygame.display.set_mode((1080, 720))
 # Background
 background = pygame.image.load("assets/bg.jpg")
 
+# Hero
+hero = Char(10)
+
 running = True
 while running:
     # Background
     window.blit(background, (0, 0))
+
+    # Hero
+    window.blit(hero.image, hero.rect)
 
     # Update
     pygame.display.flip()
@@ -22,3 +29,7 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: # If the user exits the window
             running = False
+    
+    press = pygame.key.get_pressed()
+    if press[pygame.K_RIGHT]:
+        hero.rect.x += 0.5
