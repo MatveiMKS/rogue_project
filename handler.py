5af7a58aef884510b548212f6f45a786a6133996
Maksimenka Matvei <70953270+MatveiBG@users.noreply.github.com>
2023-05-23 12:36:12 +0000

    add throw

--- handler.py
+++ handler.py
@@ -1,5 +1,7 @@
 ''' Handler module for the game.'''
 
+import Coord
+import Creature as cr
 import theGame
 
 def heal(creature, strength):
@@ -15,6 +17,17 @@ def teleport(creature, unique):
     theGame.theGame()._floor.put(destination, creature)
     return unique
 
-def throw(power, loss):
-    """Throw an object"""
-    pass
+def throw(direction, power, start_pos):
+    """Returns the position of the thrown object"""
+    pos_x = start_pos.x + direction.x * power
+    pos_y = start_pos.y + direction.y * power
+    pos = Coord.Coord(pos_x, pos_y)
+    return pos
+
+def shoot(hero, direction, power, damage):
+    """Shoots a projectile at the creature"""
+    for i in range(power):
+        pos = throw(direction, i, theGame.theGame()._floor.pos(hero))
+        if isinstance(theGame.theGame()._floor.get(pos), cr.Creature)):
+            theGame.theGame()._floor.get(pos).hp -= damage
+            return True
