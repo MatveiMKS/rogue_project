f9d8bf1d44597e92b5bbf72fd965d939924af2e7
theobilou <theodibona@gmail.com>
2023-05-30 13:50:07 +0200

    visu 2.0

--- main.py
+++ main.py
@@ -1,7 +1,33 @@
+<<<<<<< HEAD
 from utils import _find_getch
 import theGame
 from visuel import * 
 
 theGame.theGame().play1()
 
+
+=======
+from utils import _find_getch
+import theGame
+from visuel import * 
+
+
+getch = _find_getch()
+theGame.theGame().play2()
+
+pygame.init()
+
+running = True
+while running:
+    # Background
+    window.blit(background, (0, 0))
+
+    # Hero
+    window.blit(hero.image, hero.rect)
+    
+
+    # Update
+    pygame.display.flip()
+
+    for event in pygame.event.get():
 
