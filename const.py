06d6c033f9b44885be528f24da138f6ebead1654
Maksimenka Matvei <matvei.maksimenka@etu.univ-cotedazur.fr>
2023-06-03 01:45:12 +0200

    add new images

--- const.py
+++ const.py
@@ -1,19 +1,41 @@
-
+##FLOOR##
 image_sol = "assets/floor_tiles.jpg"
 image_sol_2 = "assets/floor_tiles_2.jpg"
 image_sol_3 = "assets/floor_tiles_3.jpg"
 image_sol_4 = "assets/floor_tiles_4.jpg"
 
+##HERO##
 image_hero = "assets/robot_down.jpg"
 
+##MONSTERS##
 image_monsters = "assets/monster.png"
-image_stairs = "assets/stairs.jpg"
-image_potion = "assets/potion.png"
-image_gold = "assets/gold.png"
-image_throwing = "assets/bow.png"
-image_epee = "assets/epee.jpg"
 image_bat = "assets/bat.jpg"
 image_weak_blob = "assets/blob_weak.jpg"
+
+##ELEMENTS##
+image_stairs = "assets/stairs.png"
+image_potion = "assets/potion.png"
+image_gold = "assets/gold.png"
+
+##WEAPONS##
+image_throwing = "assets/bow.png"
+image_epee = "assets/epee.jpg"
+
+###ARMOR##
+image_helmet = "assets/helmet.png"
+image_chest = "assets/chest.png"
+image_pants = "assets/pants.png"
+image_boots = "assets/boots.png"
+
+image_mid_helmet = "assets/mid_helmet.png"
+image_mid_chest = "assets/mid_chest.png"
+image_mid_pants = "assets/mid_pants.png"
+image_mid_boots = "assets/mid_boots.png"
+
+image_rare_helmet = "assets/rare_helmet.png"
+image_rare_chest = "assets/rare_chest.png"
+image_rare_pants = "assets/rare_pants.png"
+image_rare_boots = "assets/rare_boots.png"
 
 elem_type = {"Goblin" :  image_monsters,
                 "Blob" :  image_monsters,
@@ -40,5 +62,17 @@ elem_type = {"Goblin" :  image_monsters,
                 "dagger":  image_epee,
                 "sword":  image_epee,
                 "longsword":  image_epee,
-                "axe":  image_epee
+                "axe":  image_epee,
+                'leather helmet' : image_helmet,
+                'leather chest' : image_chest,
+                'leather legs' : image_pants,
+                'leather boots' : image_boots,
+                'iron helmet' : image_mid_helmet,
+                'iron chest' : image_mid_chest,
+                'iron legs' : image_mid_pants,
+                'iron boots' : image_mid_boots,
+                'steel helmet' : image_rare_helmet,
+                'steel chest' : image_rare_chest,
+                'steel legs' : image_rare_pants,
+                'steel boots' : image_rare_boots,
                 }
